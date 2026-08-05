33bbde5982d3fc1d30c5240fa63521a8dc396c4d
WietekeNijmegen <wietekenijmegen@gmail.com>
2026-08-05 15:21:16 +0200

    feat: add new project type ts-ap to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './project-cdk-app';
 export * from './project-cdk-lib';
 export * from './project-jsii-lib';
 export * from './project-ts-lib';
+export * from './project-ts-app';
 export { GemeenteNijmegenOptions } from './shared';
 
